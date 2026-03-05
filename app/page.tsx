5590d4461a4fe7b57ab3657654cedecb2b14f5f1
Shashwat Sheth <67869200+ShashwatSheth27@users.noreply.github.com>
2026-03-05 16:48:05 +0530

    fix page syntax

--- app/page.tsx
+++ app/page.tsx
@@ -1,23 +1,9 @@
-import Navbar from "./components/Navbar";
-import Hero from "./components/Hero";
-// import Services from "./components/Services";
-import Skills from "./components/Skills";
-import Projects from "./components/Projects";
-import Contact from "./components/Contact";
-import InstallScreen from "./components/InstallScreen";
+
 
 const Home = () => {
   return (
     <div className="min-h-screen bg-background">
-      {/* <InstallScreen />
-      <Navbar />
-      <Hero /> */}
-      {/* <Services /> */}
-      {/* <Skills />
-      <Projects />
-      <Contact /> */}
-        return <h1>Krunika Portfolio 🚀</h1>;
-
+      <h1>Krunika Portfolio 🚀</h1>
     </div>
   );
 };
